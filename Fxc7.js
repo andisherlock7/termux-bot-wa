28b42ecea181505813ccac8aafa6f8ced0dcb6ae
andisherlock7 <82256420+andisherlock7@users.noreply.github.com>
2021-04-10 15:07:03 +0800

    Update Fxc7.js

--- Fxc7.js
+++ Fxc7.js
@@ -107,9 +107,9 @@ const { wait, simih, getBuffer, h2k, banner, generateMessageID, getGroupAdmins, 
 
 const vcard = 'BEGIN:VCARD\n'
             + 'VERSION:3.0\n'
-            + 'FN:Farhan\n'
-            + 'ORG:Owner FXC7;\n'
-            + 'TEL;type=CELL;type=VOICE;waid=628311800241:+62 831-1800-241\n'
+            + 'FN:Andi\n'
+            + 'ORG:Owner Sherlock;\n'
+            + 'TEL;type=CELL;type=VOICE;waid=6285157339602:+62 851-5733-9602\n'
             + 'END:VCARD'
 
 function kyun(seconds){
